e7132612287ea385194aeb9700e73abf80d1a924
KathishR <114051814+KathishR@users.noreply.github.com>
2024-04-08 18:06:13 +0530

    login pages added

--- cwms/signin.php
+++ cwms/signin.php
@@ -1,200 +1,68 @@
 <!DOCTYPE html>
-<!---Coding By CodingLab | www.codinglabweb.com--->
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <!--<title>Registration Form in HTML CSS</title>-->
-<style>
-    /* Import Google font - Poppins */
-    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
-    * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: "Poppins", sans-serif;
-    }
-    body {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 20px;
-    background: rgb(130, 106, 251);
-    }
-    .container {
-    position: relative;
-    max-width: 700px;
-    width: 100%;
-    background: #fff;
-    padding: 25px;
-    border-radius: 8px;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
-    }
-    .container header {
-    font-size: 1.5rem;
-    color: #333;
-    font-weight: 500;
-    text-align: center;
-    }
-    .container .form {
-    margin-top: 30px;
-    }
-    .form .input-box {
-    width: 100%;
-    margin-top: 20px;
-    }
-    .input-box label {
-    color: #333;
-    }
-    .form :where(.input-box input, .select-box) {
-    position: relative;
-    height: 50px;
-    width: 100%;
-    outline: none;
-    font-size: 1rem;
-    color: #707070;
-    margin-top: 8px;
-    border: 1px solid #ddd;
-    border-radius: 6px;
-    padding: 0 15px;
-    }
-    .input-box input:focus {
-    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.1);
-    }
-    .form .column {
-    display: flex;
-    column-gap: 15px;
-    }
-    .form .gender-box {
-    margin-top: 20px;
-    }
-    .gender-box h3 {
-    color: #333;
-    font-size: 1rem;
-    font-weight: 400;
-    margin-bottom: 8px;
-    }
-    .form :where(.gender-option, .gender) {
-    display: flex;
-    align-items: center;
-    column-gap: 50px;
-    flex-wrap: wrap;
-    }
-    .form .gender {
-    column-gap: 5px;
-    }
-    .gender input {
-    accent-color: rgb(130, 106, 251);
-    }
-    .form :where(.gender input, .gender label) {
-    cursor: pointer;
-    }
-    .gender label {
-    color: #707070;
-    }
-    .address :where(input, .select-box) {
-    margin-top: 15px;
-    }
-    .select-box select {
-    height: 100%;
-    width: 100%;
-    outline: none;
-    border: none;
-    color: #707070;
-    font-size: 1rem;
-    }
-    .form button {
-    height: 55px;
-    width: 100%;
-    color: #fff;
-    font-size: 1rem;
-    font-weight: 400;
-    margin-top: 30px;
-    border: none;
-    cursor: pointer;
-    transition: all 0.2s ease;
-    background: rgb(130, 106, 251);
-    }
-    .form button:hover {
-    background: rgb(88, 56, 250);
-    }
-    /*Responsive*/
-    @media screen and (max-width: 500px) {
-    .form .column {
-        flex-wrap: wrap;
-    }
-    .form :where(.gender-option, .gender) {
-        row-gap: 15px;
-    }
-    }
-</style>
-  </head>
-  <body>
-    <section class="container">
-      <header>Registration Form</header>
-      <form action="#" class="form">
-        <div class="input-box">
-          <label>Full Name</label>
-          <input type="text" placeholder="Enter full name" required />
-        </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
 
-        <div class="input-box">
-          <label>Email Address</label>
-          <input type="text" placeholder="Enter email address" required />
-        </div>
+    <title>Document</title>
+    <style>
+      
+    </style>
+</head>
+<body>
+<section class="flex flex-col md:flex-row h-screen items-center">
 
-        <div class="column">
-          <div class="input-box">
-            <label>Phone Number</label>
-            <input type="number" placeholder="Enter phone number" required />
+<div class="bg-blue-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
+  <img src="img/carwash.jpg" alt="" class="w-full h-full object-cover">
+</div>
+
+<div class="bg-gray-300	 w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12
+      flex items-center justify-center">
+
+  <div class="w-full h-100">
+
+    <h1 class="text-xl md:text-2xl font-bold leading-tight mt-12">Log in to your account</h1>
+
+    <form class="mt-6" action="validate.php" method="POST">
+      <div>
+        <label class="block text-gray-700">Email Address</label>
+        <input type="email" name="mail" id="" placeholder="Enter Email Address" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none" autofocus autocomplete required>
+      </div>
+
+      <div class="mt-4">
+        <label class="block text-gray-700">Password</label>
+        <input type="password" name="pass" id="" placeholder="Enter Password" minlength="6" class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+              focus:bg-white focus:outline-none" required>
+      </div>
+
+      <div class="text-right mt-2">
+        <a href="#" class="text-sm font-semibold text-gray-700 hover:text-blue-700 focus:text-blue-700">Forgot Password?</a>
+      </div>
+
+      <button type="submit" class="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+            px-4 py-3 mt-6">Log In</button>
+    </form>
+
+    <hr class="my-6 border-gray-300 w-full">
+
+    <!-- <button type="button" class="w-full block bg-white hover:bg-gray-100 focus:bg-gray-100 text-gray-900 font-semibold rounded-lg px-4 py-3 border border-gray-300">
+          <div class="flex items-center justify-center">
+          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48"><defs><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></defs><clipPath id="b"><use xlink:href="#a" overflow="visible"/></clipPath><path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z"/><path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/><path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/><path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/></svg>
+          <span class="ml-4">
+          Log in
+          with
+          Google</span>
           </div>
-          <div class="input-box">
-            <label>Birth Date</label>
-            <input type="date" placeholder="Enter birth date" required />
-          </div>
-        </div>
-        <div class="gender-box">
-          <h3>Gender</h3>
-          <div class="gender-option">
-            <div class="gender">
-              <input type="radio" id="check-male" name="gender" checked />
-              <label for="check-male">male</label>
-            </div>
-            <div class="gender">
-              <input type="radio" id="check-female" name="gender" />
-              <label for="check-female">Female</label>
-            </div>
-            <div class="gender">
-              <input type="radio" id="check-other" name="gender" />
-              <label for="check-other">prefer not to say</label>
-            </div>
-          </div>
-        </div>
-        <div class="input-box address">
-          <label>Address</label>
-          <input type="text" placeholder="Enter street address" required />
-          <input type="text" placeholder="Enter street address line 2" required />
-          <div class="column">
-            <div class="select-box">
-              <select>
-                <option hidden>Country</option>
-                <option>America</option>
-                <option>Japan</option>
-                <option>India</option>
-                <option>Nepal</option>
-              </select>
-            </div>
-            <input type="text" placeholder="Enter your city" required />
-          </div>
-          <div class="column">
-            <input type="text" placeholder="Enter your region" required />
-            <input type="number" placeholder="Enter postal code" required />
-          </div>
-        </div>
-        <button>Submit</button>
-      </form>
-    </section>
-  </body>
+        </button> -->
+
+    <p class="mt-8">Need an account? <a href="signup.php" class="text-blue-500 hover:text-blue-700 font-semibold">Create an 
+            account</a></p>
+
+    <p class="text-sm text-gray-500 mt-12">&copy; 2020 Abstract UI - All Rights Reserved.</p>
+  </div>
+</div>
+
+</section>    
+</body>
 </html>
---
+++ cwms/signupcheck.php
@@ -0,0 +1,42 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'cwmsdb';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phno = $_POST['phno'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$address = $_POST['address'];
+$pass = $_POST['password'];
+
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("INSERT INTO userinfo (name, email, phno, dob, gender, address, password) VALUES (:name, :email, :phno, :dob, :gender, :address, :password)");
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':phno', $phno);
+    $stmt->bindParam(':dob', $dob);
+    $stmt->bindParam(':gender', $gender);
+    $stmt->bindParam(':address', $address);
+    $stmt->bindParam(':password', $pass);
+
+    $stmt->execute();
+    $lastInsertedUserId = $conn->lastInsertId();
+
+    $stmt = $conn->prepare("INSERT INTO authuser(id , name , password) VALUES( :id , :name , :password )");
+    $stmt->bindParam(':id' ,$lastInsertedUserId );
+    $stmt->bindParam(':name' ,$name);
+    $stmt->bindParam(':password' , $pass);
+    $stmt->execute();
+
+    header("Location: index.php");
+    exit();
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
